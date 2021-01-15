e0b8ede82063adb7b23984a5944709bf69c49bc2
J. Morningstar <HolidayExplanation@Gmail.com>
2021-01-15 22:28:45 +0100

    *

--- webservice/src/components/pages/Page2.vue
+++ webservice/src/components/pages/Page2.vue
@@ -1,15 +1,35 @@
 <template>
-  <div>
-      <p>Page two</p>
+  <div id="pageTwo">
+      <div id="job">
+          <div id="job_sentence">
+              <p>Job sentence</p>
+          </div>
+          <div id="job_selector">
+            <Select :list="['MIT', 'Hardvard', 'Oxford',
+            'MIT', 'Hardvard', 'Oxford',
+            'MIT', 'Hardvard', 'Oxford',
+            'MIT', 'Hardvard', 'Oxford',
+            'MIT', 'Hardvard', 'Oxford',
+            'MIT', 'Hardvard', 'Oxford',
+            'MIT', 'Hardvard', 'Oxford',
+            'MIT', 'Hardvard', 'Oxford']" />
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
-export default {
+import Select from '@/components/parts/Select'
 
+export default {
+    components: {
+        Select
+    }
 }
 </script>
 
 <style>
-
+#job_selector {
+    margin-left: 50px;
+}
 </style>
---
+++ webservice/src/components/parts/Select.vue
@@ -0,0 +1,150 @@
+<template>
+<div id="Selector">
+  <div id="search">
+    <div id="input">
+        <input type="text" placeholder="Job Titel"
+        v-model="selected"
+        @focus="showList()"
+        @keyup="query()">
+    </div>
+    <div id="icons">
+      <img id="close" src="@/assets/icons/x.svg" alt="">
+      <img id="toggle" src="@/assets/icons/arrow.svg" alt=""
+      :class="{listOpen: listVisible}"
+      @click="toggleList()">
+    </div>
+  </div>
+  <div id="list" v-show="listVisible">
+    <ul>
+        <li v-for="(item, i) in list" :key="i"
+        @click="selectItem(item)">
+          <p>{{ item }}</p>
+        </li>
+    </ul>
+  </div>
+  
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['list'],
+  data() {
+    return {
+      listVisible: false,
+      selected: null
+    }
+  },
+  methods: {
+    query() {
+      const z = this
+      const filtered = z.list.filter(function(tag) {
+        return tag.toLowerCase().indexOf(z.selected.toLowerCase()) >= 0;
+      })
+
+      console.log(filtered)
+    },
+    toggleList() {
+      this.listVisible = !this.listVisible
+    },
+    showList() {
+      this.listVisible = true
+    },
+    hideList() {
+      this.listVisible = false
+    },
+    selectItem(item) {
+      this.selected = item
+
+      this.hideList()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/mixins/centerY';
+
+#search {
+  position: relative;
+  #icons {
+    position: absolute;
+    top: 7px;
+    margin-right: 10px;
+    right: 0;
+    img {
+      margin-right: 7px;
+      padding: 3px;
+      border-radius: 5px;
+      cursor: pointer;
+      &:hover {
+        transition: 0.2s ease;
+        transform: scale(1.1);
+      }
+    }
+    #close {
+      background-color: rgba(200, 0, 0, 0.4);
+    }
+    #toggle {
+      transform: rotate(-90deg);
+      background-color: rgba(0, 0, 200, 0.4);
+    }
+  }
+}
+
+.listOpen {
+  transition: .2s ease;
+  transform: rotate(90deg) !important;
+}
+
+#Selector {
+  width: 300px;
+}
+
+img {
+  height: 15px;
+  width: 15px;
+  fill: rgba(0, 0, 0, 0.5);
+}
+
+input {
+  width: calc(100% - 5px);
+  margin: 0;
+  height: 30px;
+  border-radius: 5px;
+  border: 1px solid rgba(0, 0, 0, 0.5);
+  outline: none;
+  text-align: center;
+  font-size: 17px;
+}
+
+ul {
+  margin: auto;
+  margin: 0;
+  background-color: white;
+  border: 1px solid rgba(0, 0, 0, 0.5);
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
+  padding: 0;
+  max-height: 500px;
+  overflow-y: auto !important;
+  li {
+    padding: 0;
+    width: 100%;
+    height: 32px;
+    position: relative;
+    cursor: pointer;
+    &:hover {
+      transition: .2s ease;
+      background-color: rgb(100, 159, 226);
+    }
+    p {
+      @include centerY;
+      position: absolute;
+      font-weight: bold;
+      margin-left: 20px;
+    }
+  }
+}
+
+</style>
